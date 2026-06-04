253bc334040e5d98204dbc6c4ed9346d32fa2874
qwerttt0745 <olexiy2005@gmail.com>
2026-06-04 17:13:03 +0300

    fix k8s

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,12 +15,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_prometheus",
     "apps.accounts",
     "apps.applications",
     "apps.reports",
 ]
 
 MIDDLEWARE = [
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -28,6 +30,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("-/", include("django_prometheus.urls")),  
     path("", include("apps.applications.urls")),
     path("", include("apps.accounts.urls")),
     path("reports/", include("apps.reports.urls")),
@@ -13,6 +14,5 @@ urlpatterns = [
 if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
-
         urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
